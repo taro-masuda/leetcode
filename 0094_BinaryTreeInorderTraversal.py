12387de207e67b2477ca3fe5805b1da0ef743580
Taro Masuda <57311096+taro-masuda@users.noreply.github.com>
2019-11-24 22:27:55 +0900

    Create 0094_BinaryTreeInorderTraversal.py

---
+++ 0094_BinaryTreeInorderTraversal.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        if root == None:
+            return []
+        out = []
+        
+        if root.left != None:
+            out.extend(self.inorderTraversal(root.left))
+            
+        out.append(root.val)
+        
+        if root.right != None:
+            out.extend(self.inorderTraversal(root.right))
+                
+        return out
