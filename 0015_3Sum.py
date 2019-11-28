d547e0b33137684ec3140906d20bd89bea3aa6b2
Taro Masuda <57311096+taro-masuda@users.noreply.github.com>
2019-11-28 23:51:00 +0900

    Update 0015_3Sum.py

--- 0015_3Sum.py
+++ 0015_3Sum.py
@@ -1,20 +1,13 @@
+import bisect
+
 class Solution:
     
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        complement = []
-        #print(nums)
         out = []
-        for n in nums:
-            complement.append(target - n) 
-        for i,n in enumerate(nums):
-            try:
-                idx = complement.index(n)
-            except ValueError:
-                idx = None
-            #print(complement)
-            #print(n, idx, i)
-            if idx != None and idx != i:
-                out.append([nums[idx], nums[i], -target])
+        while len(nums) >= 2:
+            if nums.count(-target-nums[0]) > 0:
+                out.append([-target-nums[0], nums[0], target])
+            nums.pop(0)
         return out
                 
     def threeSum(self, nums: List[int]) -> List[List[int]]:
@@ -23,22 +16,17 @@ class Solution:
         nums.sort()
         
         out = []
-        print('a')
         
         i = 0
-        #for i,n in enumerate(nums):
         while len(nums) >= 3:
             if nums[0] > 0:
                 break
-            l_twosum = self.twoSum(nums[1:], -nums[0])
+            l_twosum = self.twoSum(nums[1:], nums[0])
             if l_twosum != None:
-                #print(l_twosum, i, n)
                 out.extend(l_twosum)
             nums.pop(0)
             
-        print('b')
         for i,l in enumerate(out):
             out[i] = sorted(l)
-        print('c')
         out = list(map(list, set(map(tuple, out))))
         return out
