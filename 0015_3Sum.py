1a97c6dcd92317960d87af52a99b4a5530328235
Taro Masuda <57311096+taro-masuda@users.noreply.github.com>
2019-11-28 21:49:32 +0900

    Update 0015_3Sum.py

--- 0015_3Sum.py
+++ 0015_3Sum.py
@@ -1,6 +1,4 @@
 class Solution:
-    def my_index_multi(l, x):
-        return [i for i, _x in enumerate(l) if _x == x]
     
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         complement = []
@@ -16,7 +14,7 @@ class Solution:
             #print(complement)
             #print(n, idx, i)
             if idx != None and idx != i:
-                out.append([idx, i])
+                out.append([nums[idx], nums[i], -target])
         return out
                 
     def threeSum(self, nums: List[int]) -> List[List[int]]:
@@ -26,6 +24,7 @@ class Solution:
         out = []
         
         for i,n in enumerate(nums):
+            
             if i == 0:
                 #print(nums[i+1:], n)
                 l_twosum = self.twoSum(nums[i+1:], -n)
@@ -37,15 +36,10 @@ class Solution:
                 l_twosum = self.twoSum(nums[:i] + nums[i+1:], -n)
             if l_twosum != None:
                 #print(l_twosum, i, n)
-                for j,l in enumerate(l_twosum):
-                    if l_twosum[j][0] >= i:
-                        l_twosum[j][0] += 1
-                    if l_twosum[j][1] >= i:
-                        l_twosum[j][1] += 1
-                    out.append([nums[l_twosum[j][0]], nums[l_twosum[j][1]], n])
+                out.extend(l_twosum)
         
         for i,l in enumerate(out):
             out[i] = sorted(l)
-        #print(out)
+        print(out)
         out = list(map(list, set(map(tuple, out))))
         return out
