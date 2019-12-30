5bce06bf17e13e994855d0f571590930327c1dbb
Taro Masuda <57311096+taro-masuda@users.noreply.github.com>
2019-12-30 20:55:07 +0900

    Update 0029_DivideTwoIntegers.cpp

--- 0029_DivideTwoIntegers.cpp
+++ 0029_DivideTwoIntegers.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        int sign;
-        if (dividend > 0) {
-            dividend = - std::abs(dividend);
-            sign = 1;
-        }else {sign = -1;}
-        if (divisor > 0) {
-            divisor = - std::abs(divisor);
-            if(sign == -1) sign = -1;
-            else sign = 1;
-        }else {
-            if (sign == -1) sign = 1;
-            else sign = -1;
-        }
-        int out = 0;
-        while (dividend <= divisor){
+        int dividedCount = 0;
+        
+        if (dividend == - pow(2,31) && divisor == -1) return pow(2,31) - 1;
+        else if (dividend == - pow(2,31) && divisor > 0) {
+            dividend += divisor;
+            dividedCount++;
+        } else if (dividend == - pow(2,31) && divisor < 0) {
             dividend -= divisor;
-            out += 1;
+            dividedCount++;
         }
-        if (sign == -1) return -out;
-        else return out;
+        
+        if (dividend > 0 && divisor > 0) {
+            while (dividend >= divisor) {
+                dividend -= divisor;
+                dividedCount++;
+            }
+            return dividedCount;
+        } else if (dividend > 0 && divisor < 0) {
+            while (-dividend <= divisor) {
+                dividend += divisor;
+                dividedCount++;
+            }
+            return -dividedCount;
+        } else if (dividend < 0 && divisor > 0) {
+            while (-dividend >= divisor) {
+                dividend += divisor;
+                dividedCount++;
+            }
+            return -dividedCount;
+        } else if (dividend < 0 && divisor < 0) {
+            while (-dividend >= -divisor) {
+                dividend -= divisor;
+                dividedCount++;
+            }
+            return dividedCount;
+        } else return 0; // dividend == 0 && divisor != 0
     }
 };
