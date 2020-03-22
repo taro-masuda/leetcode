c9790333c4f25fc3fb4abb201cf650a9c8c1f369
Taro Masuda <57311096+taro-masuda@users.noreply.github.com>
2020-03-22 23:17:33 +0900

    Update 0001_TwoSum.py

--- 0001_TwoSum.py
+++ 0001_TwoSum.py
@@ -1,12 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        complement = []
-        for n in nums:
-            complement.append(target - n) 
+        complement = {}
         for i,n in enumerate(nums):
-            try:
-                idx = complement.index(n)
-            except ValueError:
-                idx = None
-            if idx != None and idx != i:
-                return [idx, i]
+            complement[target-n] = i
+        for i,n in enumerate(nums):
+            if n in complement:
+                if complement[n] != i:
+                    return [i, complement[n]]
