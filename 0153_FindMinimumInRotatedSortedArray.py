5cadff8863b7ea7084c08e1edc060802ced4ec83
Taro Masuda <57311096+taro-masuda@users.noreply.github.com>
2020-05-09 01:09:07 +0900

    Update 0153_FindMinimumInRotatedSortedArray.py

--- 0153_FindMinimumInRotatedSortedArray.py
+++ 0153_FindMinimumInRotatedSortedArray.py
@@ -5,7 +5,16 @@ class Solution:
             return nums[0]
         if n == 2:
             return min(nums[0], nums[1])
-        for i in range(len(nums)-1):
-            if nums[i] > nums[i+1]:
-                return nums[i+1]
-        return nums[0]
+        left = 0
+        right = n
+        while left < right:
+            mid = int((left + right) / 2)
+            if mid + 1 < n:
+                if nums[mid] > nums[mid+1]:
+                    return nums[mid+1]
+                elif nums[left] < nums[mid]:
+                    left = mid
+                else:
+                    right = mid
+            else:
+                return nums[0]
