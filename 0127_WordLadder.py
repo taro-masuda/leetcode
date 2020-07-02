53d9f0b255e2a620011bb2c2f6302801ef185946
Taro Masuda <taro.massu@gmail.com>
2020-07-03 01:01:09 +0900

    Update 0127_WordLadder.py

--- 0127_WordLadder.py
+++ 0127_WordLadder.py
@@ -1,39 +1,42 @@
-import copy
-
-class Solution:
-    def levenshtein_distance(self, w1: str, w2: str) -> int:
-        count = 0
-        for i in range(len(w1)):
-            if w1[i] != w2[i]:
-                count += 1
-        #print("ld", count)
-        return count
+class Solution:   
+    def __init__(self):
+        self.dic = {}
+    
+    def makeAdjacentList(self, wordList) -> None:
+        for word in wordList:
+            L = len(word)
+            for i in range(L):
+                key = word[:i] + "*" + word[i+1:]
+                if key not in self.dic:
+                    self.dic[key] = [word]
+                else:
+                    self.dic[key].append(word)
+                
     
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        #print("beginword: ", beginWord, "endWord: ", endWord)
-        if beginWord == endWord:
-            return 0
-        elif not (endWord in wordList):
+        q = [(beginWord,1)]
+        visited = set()
+        now_word = beginWord
+        word_set = set(wordList)
+        if endWord not in word_set:
             return 0
         
-        count = []
-        for w in wordList:
-            if self.levenshtein_distance(beginWord, w) == 1:
-                current_count = 2
-                
-                if w == endWord:
-                    #print("same word appears so append current count")
-                    count.append(current_count)
+        self.makeAdjacentList(wordList)
+        # Breadth First Search
+        while len(q) > 0:
+            top_word, top_dist = q[0]
+            del q[0]
+            L = len(top_word)
+            for i in range(L):
+                key = top_word[:i] + "*" + top_word[i+1:]
+                if not key in self.dic:
                     continue
-                    
-                wl = copy.deepcopy(wordList)
-                wl.remove(w)
-                ll = self.ladderLength(w, endWord, wl)
-                if ll > 0:
-                    current_count += ll-1
-                    count.append(current_count)
-        #print("ladderlength", count)
-        if len(count) == 0:
-            return 0
-        else:
-            return min(count)
+                for word in self.dic[key]:
+                    if word == top_word:
+                        continue
+                    elif word == endWord:
+                        return top_dist+1
+                    elif word not in visited:
+                        visited.add(word)
+                        q.append((word,top_dist+1))
+        return 0
