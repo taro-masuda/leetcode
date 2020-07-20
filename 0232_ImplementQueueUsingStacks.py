84c54d2bbeead7418a8641f783b1c7b43f4603b5
Taro Masuda <taro.massu@gmail.com>
2020-07-21 00:52:47 +0900

    Create 0232_ImplementQueueUsingStacks.py

---
+++ 0232_ImplementQueueUsingStacks.py
@@ -0,0 +1,51 @@
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.qmain = []
+        self.qsub = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        if len(self.qmain) == 0:
+            self.qmain.append(x)
+        else:
+            while len(self.qmain) > 0:
+                self.qsub.append(self.qmain[-1])
+                self.qmain.pop(-1)
+            self.qsub.append(x)
+            while len(self.qsub) > 0:
+                self.qmain.append(self.qsub[-1])
+                self.qsub.pop(-1)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        val = self.qmain[-1]
+        self.qmain.pop(-1)
+        return val
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        return self.qmain[-1]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return len(self.qmain) == 0
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
