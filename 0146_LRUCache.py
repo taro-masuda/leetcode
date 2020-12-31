a7ef6bcf6fe6db8a1bdcf3df66503eebe7017ef0
Taro Masuda <taro.masuda.jp@gmail.com>
2021-01-01 00:43:34 +0900

    Update 0146_LRUCache.py

--- 0146_LRUCache.py
+++ 0146_LRUCache.py
@@ -1,27 +1,29 @@
 class LRUCache:
 
     def __init__(self, capacity: int):
+        self.dic = {}
         self.capacity = capacity
-        self.keys = []
-        self.values = []
+        self.q = []
 
     def get(self, key: int) -> int:
-        if key in self.keys:
-            v = self.values[self.keys.index(key)]
-            self.put(key,v)
-            return v
+        if key in self.dic:
+            self.q.remove(key)
+            self.q.append(key)
+            return self.dic[key]
         else:
             return -1
 
     def put(self, key: int, value: int) -> None:
-        if key in self.keys:
-            self.values.pop(self.keys.index(key))
-            self.keys.remove(key)
-        if len(self.keys) >= self.capacity:
-            self.keys.pop(0)
-            self.values.pop(0)
-        self.keys.append(key)
-        self.values.append(value)
+        if key in self.dic:
+            self.q.remove(key)
+        else:
+            if len(self.q) == self.capacity:
+                self.dic.pop(self.q[0])
+                self.q.pop(0)
+        self.q.append(key)
+        self.dic[key] = value
+        
+        
 
 
 # Your LRUCache object will be instantiated and called as such:
