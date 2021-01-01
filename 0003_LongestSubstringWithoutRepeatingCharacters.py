54447154cc2da5e28c6f42c0541b0923875da806
Taro Masuda <taro.masuda.jp@gmail.com>
2021-01-02 01:40:38 +0900

    Update 0003_LongestSubstringWithoutRepeatingCharacters.py

--- 0003_LongestSubstringWithoutRepeatingCharacters.py
+++ 0003_LongestSubstringWithoutRepeatingCharacters.py
@@ -1,20 +1,29 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        maxLen = 0
-        for i in range(len(s)):
-            charSet = set()
-            currLen = 0
-            j = i
-            isBreak = False
-            while j < len(s):
-                if s[j] not in charSet:
-                    charSet.add(s[j])
-                    currLen += 1
-                    j += 1
+        dic = {}
+        if len(s) > 0: max_len = 1
+        else: max_len = 0
+        cur_len = 0
+        for i,c in enumerate(s):
+            if c not in dic:
+                dic[c] = i
+                cur_len += 1
+                max_len = max(max_len, cur_len)
+            elif dic[c] == -1:
+                dic[c] = i
+                cur_len += 1
+                max_len = max(max_len, cur_len)
+            else:
+                if i-dic[c] == 1:
+                    for key in dic:
+                        dic[key] = -1
+                    cur_len = 1
+                    max_len = max(max_len, cur_len)
                 else:
-                    maxLen = max(currLen, maxLen)
-                    isBreak = True
-                    break
-            if not isBreak:
-                maxLen = max(maxLen, j-i)
-        return maxLen
+                    for key in dic:
+                        if dic[key] < dic[c]: dic[key] = -1
+                    max_len = max(max_len, cur_len)
+                    cur_len = i-dic[c]
+                dic[c] = i
+            
+        return max(max_len, cur_len)
