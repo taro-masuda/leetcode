39739e9fec7c66513b114c740ef982ccc09dc39f
Taro Masuda <taro.masuda.jp@gmail.com>
2021-01-03 15:22:36 +0900

    Update 0053_MaximumSubarray.py

--- 0053_MaximumSubarray.py
+++ 0053_MaximumSubarray.py
@@ -1,7 +1,6 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        if len(nums) == 0: return -2147483648
-        elif len(nums) == 1: return nums[0]
+        if len(nums) == 1: return nums[0]
         
         max_sum = nums[0]
         previous_sum = nums[0]
